cee335f04f0749066dcc1e9fa85d882133c65397
hasharas <hasharamsankalpam@gmail.com>
2024-07-26 16:30:45 +0530

    create header js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './header/Header.js';
+
 
 function App() {
   return (
-   <h1 class="text-center">hello world</h1>
+   <Header />
   );
 }
 
