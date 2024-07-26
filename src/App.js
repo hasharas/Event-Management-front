e73fb298652d3e2d196e3b15cdb121bb66549c76
hasharas <hasharamsankalpam@gmail.com>
2024-07-26 23:51:51 +0530

    Create dashbordjs file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
+import { Route,Routes } from 'react-router-dom';
 import './App.css';
-import Header from './header/Header.js';
+import Header from './pages/header/Header.js';
+import Dashboard from './pages/dashboard/Dashboard.js';
 
 
 function App() {
   return (
+    <>
    <Header />
+   <Routes>
+        <Route path="/" element={<Dashboard />} />
+   </Routes>
+   </>
   );
 }
 
