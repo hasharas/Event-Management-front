092394f4b24091c379b4c445f47904cae15ee538
hasharas <hasharamsankalpam@gmail.com>
2024-07-28 12:23:00 +0530

    Create postuser js file and create some functions

--- src/pages/employee/PostUser.js
+++ src/pages/employee/PostUser.js
@@ -1,6 +1,7 @@
 
 import { useState } from "react";
 import "./PostUser.css";
+import form from "react-bootstrap/Form"
 
 const PostUser = () =>{
 
@@ -9,10 +10,67 @@ const PostUser = () =>{
             email:"",
             phone:"",
             department:""
-      })
+      });
+
+      const handleInputChange = () =>{
+            const {name, value} = event.target;
+            setFormData({
+                  ...formData,
+                  [name]:value,
+            })
+      }
 
       return(
             <>
+                  <div className="center-form">
+                        <h1>Post New Employee</h1>
+                        <form action="">
+                              <form.Group controlId = "formBasicName">
+                                    <form.Control 
+                                          type="text"
+                                          name="name"
+                                          placeholder="Enter Name"
+                                          value={formData.name}
+                                          onChange={handleInputChange}
+                                    />
+                              </form.Group>
+
+                              <form.Group controlId = "formBasicName">
+                                    <form.Control 
+                                          type="text"
+                                          name="email"
+                                          placeholder="Enter email"
+                                          value={formData.email}
+                                          onChange={handleInputChange}
+                                    />
+                              </form.Group>
+
+                              <form.Group controlId = "formBasicName">
+                                    <form.Control 
+                                          type="text"
+                                          name="phone"
+                                          placeholder="Enter phone"
+                                          value={formData.phone}
+                                          onChange={handleInputChange}
+                                    />
+                              </form.Group>
+
+                              <form.Group controlId = "formBasicName">
+                                    <form.Control 
+                                          type="text"
+                                          name="department"
+                                          placeholder="Enter department"
+                                          value={formData.department}
+                                          onChange={handleInputChange}
+                                    />
+                              </form.Group>
+
+                              <Button variants="primary" type="submit" className="w-100">
+                                    Post Employee
+                              </Button>
+
+                        </form>
+                  </div>
             </>
       )
 }
