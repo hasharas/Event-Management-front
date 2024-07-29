3a6690656e4b206e3d135cc9cc3b479359bc74bf
hasharas <hasharamsankalpam@gmail.com>
2024-07-29 23:26:20 +0530

    create employee tablee

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,9 +1,68 @@
+import { useEffect, useState } from "react";
+import "./Dashboard.css";
+import { Button, Row, Col, Container } from 'react-bootstrap';
+//import Raw from "react-bootstrap/Raw";
+//import Col from "react-bootstrap/Col";
+import Table from "react-bootstrap/Table";
+
+//import Button from "react-bootstrap/Button";
 
 const Dashboard = () =>{
+
+      const [employees, setEmployees ] = useState([]);
+
+      useEffect( ()=>{
+            const fetchEmployees = async () =>{
+                  try {
+                        const response = await fetch("http://localhost:8081/api/employees");
+                        const data = await response.json();
+
+                        setEmployees(data);
+                  } catch (error) {
+                        console.error("Error fetching employees", error.message);
+
+                        
+                  }
+            }
+            fetchEmployees();
+      }, []);
+
       return(
-            <div>
-                  <h1>Dashbord component</h1>
-            </div>
+            
+            <>
+            <Container className="mt-4">
+                  <Row >
+                        <Col >
+                              <h1 className="text-center">Employees</h1>
+                              <Table striped bordered hover responsive className="custom-table">
+                                    <thead >
+                                          <tr>
+                                                <th >Name</th>
+                                                <th>Email</th>
+                                                <th>Phone</th>
+                                                <th>Department</th>
+                                                <th>Action</th>
+                                          </tr>
+                                    </thead>
+                                    <tbody>
+                                          {employees.map((employee) => (
+                                                <tr key={employee.id} >
+                                                      <td>{employee.name}</td>
+                                                      <td>{employee.email}</td>
+                                                      <td>{employee.phone}</td>
+                                                      <td>{employee.department}</td>
+                                                      <td>
+                                                            <Button variant="outline-secondary" className="custom-bt">Update</Button>{" "}
+                                                            <Button variant="outline-danger">Delete</Button>
+                                                      </td>
+                                                </tr>
+                                          ))}
+                                    </tbody>
+                              </Table>
+                        </Col>
+                  </Row>
+            </Container>
+            </>
       )
 }
 
