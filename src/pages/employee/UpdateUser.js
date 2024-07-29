db1308a018e535c39091ac3c7f059567d5485715
hasharas <hasharamsankalpam@gmail.com>
2024-07-30 01:13:58 +0530

    create update user file and add csss fro front

--- src/pages/employee/UpdateUser.js
+++ src/pages/employee/UpdateUser.js
@@ -1,11 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./UpdateUser.css";
-
+import { Button } from 'react-bootstrap';
+import form from "react-bootstrap/Form"
 const UpdateUser = () => {
-      return (
-            <div>
 
-            </div>
+      const [formData, setFormData] = useState({
+            name: "",
+            email: "",
+            phone: "",
+            department: ""
+      });
+
+      const handleInputChange = (event) => {
+            const { name, value } = event.target;
+            setFormData({
+                  ...formData,
+                  [name]: value,
+            })
+      };
+
+
+      return (
+            <>
+                  <div className="center-form">
+                        <h1 className="h1" >Edit Employee</h1>
+                        <form >
+                              <form.Group controlId="formBasicName">
+                                    <form.Control
+                                          type="text"
+                                          name="name"
+                                          placeholder="Enter Name"
+                                          value={formData.name}
+                                          onChange={handleInputChange}
+                                    />
+                              </form.Group>
+
+                              <form.Group controlId="formBasicName">
+                                    <form.Control
+                                          type="email"
+                                          name="email"
+                                          placeholder="Enter email"
+                                          value={formData.email}
+                                          onChange={handleInputChange}
+                                    />
+                              </form.Group>
+
+                              <form.Group controlId="formBasicName">
+                                    <form.Control
+                                          type="text"
+                                          name="phone"
+                                          placeholder="Enter phone"
+                                          value={formData.phone}
+                                          onChange={handleInputChange}
+                                    />
+                              </form.Group>
+
+                              <form.Group controlId="formBasicName">
+                                    <form.Control
+                                          type="text"
+                                          name="department"
+                                          placeholder="Enter department"
+                                          value={formData.department}
+                                          onChange={handleInputChange}
+                                    />
+                              </form.Group>
+
+                              <Button type="submit" className="w-100 custom-button">
+                                    Edit Employee
+                              </Button>
+
+                        </form>
+                  </div>
+            </>
+
       );
 }
 
